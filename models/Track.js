187045855d7aad1daf24fbe6bd13b957679f95b8
okwudili onyido <qubesentertainmentmagazine@yahoo.com>
2024-06-25 11:48:41 +0100

    correct image7

--- models/Track.js
+++ models/Track.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const TrackSchema = new mongoose.Schema({
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
+  // user_id: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   required: true,
+  //   ref: "User",
+  // },
   name: {
     type: String,
     required: true,
